26cc7eba94af55dd866d30e2af20326b6eeea867
MHRabea <eng.mahmoud196@gmail.com>
2023-08-13 16:26:03 +0300

    setting upload buttons and gandling upload errors

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,15 +1,48 @@
-import React from "react";
-
-
+import React, { useState } from "react";
 
 const Upload = () => {
-    return (
-        <form action="">
-            <input type="file" id="upload" style={{display:"none"}}/>
-            <label htmlFor="upload" className="btn btn-dark btn-circle btn-x1 d-flex justify-content-center">+</label>
-        </form>
-    )
-} 
+  const [file, setFile] = useState(null);
+  const [err, setErr] = useState(null);
 
+  const types = ["image/png", "image/jpeg"];
 
-export default Upload; 
+  const handleUpload = (e) => {
+    const uploadedFile = e.target.files[0];
+
+    if (uploadedFile && types.includes(uploadedFile.type)) {
+      setFile(uploadedFile);
+      setErr(null);
+      console.log("uploaded", uploadedFile);
+    } else {
+      setFile(null);
+      setErr("image type unsupported please use (png or jpeg)");
+      console.log("image type unsupported please use {png or jpeg}");
+    }
+  };
+
+  return (
+    <form action="">
+      <input
+        type="file"
+        id="upload"
+        style={{ display: "none" }}
+        onChange={handleUpload}
+      />
+      <label
+        htmlFor="upload"
+        className="btn btn-dark btn-circle btn-x1 d-flex justify-content-center"
+      >
+        +
+      </label>
+      <div className="output">
+        {err && (
+          <div className="error">
+           {err}
+          </div>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default Upload;
